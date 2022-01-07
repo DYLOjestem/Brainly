4cdd574e27bce3f647525bf4f02f1f0e8e86dc87
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2022-01-07 14:08:41 +0100

    Create Brainlyscript.js

---
+++ Brainlyscript.js
@@ -0,0 +1,52 @@
+//code 0.1
+var element = document.getElementById("new-ad-placeholder-question-desktop");element.parentNode.removeChild(element);
+//code 0.2
+localStorage.setItem("flexible-funnel-cycle-start", 0);
+localStorage.setItem("flexible-funnel-last-access-data", 0);
+//code 0.3
+var elements = document.getElementsByClassName("sg-text sg-text--break-words brn-rich-content js-answer-content");
+for(var i = 0; i < elements.length; i++) {
+    elements[i].style.fontFamily="Comic Sans MS";
+}
+   var elements = document.getElementsByClassName("sg-text sg-text--large sg-text--bold sg-text--break-words brn-qpage-next-question-box-content__primary");
+for(var i = 0; i < elements.length; i++) {
+    elements[i].style.fontFamily="Comic Sans MS";
+}
+//code 0.4
+var elements = document.getElementsByClassName("sg-text sg-text--break-words brn-qpage-next-question-box-content__secondary");
+for(var i = 0; i < elements.length; i++) {
+    elements[i].style.fontFamily="Comic Sans MS";
+}
+//code 1.0
+var elements = document.getElementsByClassName("brn-qpage-background brn-header-subnav-expanded");
+for(var i = 0; i < elements.length; i++) {
+    elements[i].style.fontFamily="Comic Sans MS";
+}
+//code 1.1
+var elements = document.getElementsByClassName("brn-header-subnav-expanded");
+for(var i = 0; i < elements.length; i++) {
+    elements[i].style.fontFamily="Comic Sans MS";
+}
+//code 1.2
+document.getElementsByClassName("brn-common-layout-container js-ads-screening-layout").length>0&&(document.getElementsByClassName("brn-common-layout-container js-ads-screening-layout")[0].style="background-image: url('https://cdn.discordapp.com/attachments/784001945719275521/926411763040280577/3497712.jpg');");
+//code 1.3
+document.getElementsByClassName("sg-button sg-button--m sg-button--solid-mint")[0].innerHTML = "Premium Unlocked";
+//code 1.4
+document.getElementsByClassName("sg-headline sg-headline--large sg-headline--text-green-60 sg-headline--extra-bold")[0].innerHTML = "∞";
+//code 2.0
+var elems = document.querySelectorAll(".brn-expanded-bottom-banner");
+elems.forEach(function(element) {
+  element.parentNode.removeChild(element);
+});
+//hacker mode
+var x = document.getElementsByClassName("sg-box sg-box--light sg-box--padding-l TextbooksQAEntryPointFilterOptions-module__container--2HG7b")[0];x.id="menu"
+var hackermode = document.getElementById("menu");
+hackermode.parentNode.removeChild(hackermode);
+//hackermodev2
+var y = document.getElementsByClassName("BrainlyAdsPlaceholder-module__placeholder--viegT AdsBelowAnswers-module__placeholder--1wN9x brn-ads-box BrainlyAdsPlaceholder-module__placeholder--with-background--2GLnl")[0];y.id="hackermodev2"
+var hackermodev2 = document.getElementById("hackermodev2");
+hackermodev2.parentNode.removeChild(hackermodev2);
+//hackermodev3
+var z = document.getElementsByClassName("brn-ads-box brn-ads-sticky-wrapper")[0];z.id="hackermodev3"
+var hackermodev3 = document.getElementById("hackermodev3");
+hackermodev3.parentNode.removeChild(hackermodev3);
